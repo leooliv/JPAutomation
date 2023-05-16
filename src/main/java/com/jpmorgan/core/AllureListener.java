a294df23c5124e9c633580acc7ea82e02d76b124
leooliv <leonardo.dolivier@gmail.com>
2023-05-16 17:39:41 -0300

    Added Allure Reports

---
+++ src/main/java/com/jpmorgan/core/AllureListener.java
@@ -0,0 +1,68 @@
+package com.jpmorgan.core;
+
+import io.cucumber.plugin.event.EventPublisher;
+import io.cucumber.plugin.event.Status;
+import io.cucumber.plugin.event.TestCaseFinished;
+import io.qameta.allure.Allure;
+import io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm;
+import java.io.ByteArrayInputStream;
+import java.time.LocalDate;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AllureListener extends AllureCucumber7Jvm {
+
+  private static final Logger logger = LoggerFactory.getLogger(
+    AllureListener.class
+  );
+
+  @Override
+  public void setEventPublisher(EventPublisher publisher) {
+    publisher.registerHandlerFor(
+      TestCaseFinished.class,
+      testCaseFinished -> {
+        System.out.println("Test finished 1");
+        if (testCaseFinished.getResult().getStatus().is(Status.FAILED)) {
+          if (ChromeDriverFactory.getDriver() != null) System.out.println(
+            "Test finished 2"
+          );
+          Allure
+            .getLifecycle()
+            .addAttachment(
+              "image error - " + LocalDate.now(),
+              "image/png",
+              "png",
+              saveScreenShootPNG()
+            );
+          // logger.error("TEST CASE FAILED " + testCaseFinished.getTestCase().getName());
+        }
+
+        if (testCaseFinished.getResult().getStatus().is(Status.PASSED)) {
+          if (ChromeDriverFactory.getDriver() != null) System.out.println(
+            "Test finished 3"
+          );
+          Allure
+            .getLifecycle()
+            .addAttachment(
+              "print screen - " + LocalDate.now(),
+              "image/png",
+              "png",
+              saveScreenShootPNG()
+            );
+          // logger.info("TEST CASE PASSED " + testCaseFinished.getTestCase().getName());
+        }
+      }
+    );
+    super.setEventPublisher(publisher);
+  }
+
+  public ByteArrayInputStream saveScreenShootPNG() {
+    return new ByteArrayInputStream(
+      ((TakesScreenshot) ChromeDriverFactory.getDriver()).getScreenshotAs(
+          OutputType.BYTES
+        )
+    );
+  }
+}
--- src/test/java/com/jpmorgan/core/steps/StepsHooks.java
+++ src/test/java/com/jpmorgan/core/steps/StepsHooks.java
@@ -2,14 +2,38 @@ package com.jpmorgan.core.steps;
 
 import static com.jpmorgan.core.ChromeDriverFactory.getDriver;
 
+import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
+import java.io.ByteArrayInputStream;
+import java.time.LocalDate;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class StepsHooks {
 
   @Before
   public void setup() {
     getDriver().get("https://realworld.svelte.dev");
+  }
+
+  public ByteArrayInputStream saveScreenShootPNG() {
+    return new ByteArrayInputStream(
+      ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES)
+    );
+  }
+
+  @After
+  public void tearDown(Scenario scenario) {
+    String scenarioName =
+      scenario.getStatus() + " - " + scenario.getName() + LocalDate.now();
+
+    if (getDriver() != null) Allure
+      .getLifecycle()
+      .addAttachment(scenarioName, "image/png", "png", saveScreenShootPNG());
+    System.out.println("Driver after: ");
   }
 
   @AfterAll
