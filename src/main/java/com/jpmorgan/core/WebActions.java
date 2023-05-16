31d3db829a989240adcfe1ef8c57f525908c4f5f
leooliv <leonardo.dolivier@gmail.com>
2023-05-16 16:25:57 -0300

    Added logger to actions

--- src/main/java/com/jpmorgan/core/WebActions.java
+++ src/main/java/com/jpmorgan/core/WebActions.java
@@ -7,22 +7,30 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WebActions {
 
   protected static WebDriver driver;
+  private static final Logger logger = LoggerFactory.getLogger(
+    WebActions.class
+  );
 
   public WebActions() {
+    logger.info("Initializer driver ....");
     driver = ChromeDriverFactory.getDriver();
   }
 
   public WebActions click(WebElement element) {
     waitElement(element);
+    logger.info("Clicking element: " + element);
     element.click();
     return this;
   }
 
   public WebActions typeEmail(WebElement element, String email) {
+    logger.info("typing email: " + email);
     try {
       waitElement(element);
       element.sendKeys(email);
@@ -40,12 +48,14 @@ public class WebActions {
 
   public WebActions typeValue(WebElement element, String value) {
     waitElement(element);
+    logger.info("typing value" + value);
     element.sendKeys(value);
     return this;
   }
 
   public WebActions clearValue(WebElement element) {
     waitElement(element);
+    logger.info("Clearing value " + element);
     element.clear();
     return this;
   }
@@ -64,6 +74,7 @@ public class WebActions {
         ExpectedConditions.refreshed(ExpectedConditions.visibilityOf(element))
       );
     } catch (Exception e) {
+      logger.error("Could not find element as expected: ");
       throw new WebDriverException("Could not find element as expected: ", e);
     }
     return this;
@@ -71,6 +82,7 @@ public class WebActions {
 
   public WebActions waitTime(long seconds) {
     try {
+      logger.warn("wait" + "'" + seconds + "' seconds");
       Thread.sleep(seconds * 1000);
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
@@ -88,6 +100,7 @@ public class WebActions {
         )
       );
     } catch (Exception e) {
+      logger.error("Could not find element as expected: ");
       throw new WebDriverException("Could not find element as expected: ", e);
     }
   }
